3f59715f4fb5e2a44ed77fd79d0887dce70b39e0
sunouchi <t@sunouchi.jp>
2016-08-31 10:47:45 +0900

    update 10

--- 11.sh
+++
@@ -1,7 +0,0 @@
-# 11. タブをスペースに置換
-# タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
-
-# sed s/[[:cntrl:]]/\ /g hightemp.txt
-cat './hightemp.txt' | tr '\t' '\ '
-# expand -t 1 hightemp.txt
-
